48d09023e51ce7da5ff6793f21b4f42bfe69191e
Skyboss878 <elliottnathan800@gmail.com>
2025-07-07 06:29:24 -0700

    Fix Netlify build: install vite + updated netlify.toml

--- netlify-plugin-clean.sh
+++
@@ -1,6 +0,0 @@
-echo "🔥 Removing Netlify Lighthouse plugin manually..."
-
-rm -rf .netlify/plugins/node_modules/@netlify/plugin-lighthouse
-rm -rf node_modules/@netlify/plugin-lighthouse
-
-echo "✅ Lighthouse plugin manually removed."
